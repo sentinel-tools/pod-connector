1928da184fd10d235f87a27c0b61fd6db9dc3acc
therealbill <therealbill@me.com>
2015-05-06 14:58:24 -0500

    Updated to use CLI command/sbcommand library, bash completion added, readme update to reflect usage changes.

--- README.md
+++ README.md
@@ -3,42 +3,28 @@
 
 # Pod Connector
 Pod connector is a CLI Tool for extracting information for a specific pod from
-a sentinel config file or via a Redskull server.
+a sentinel config file.
 
 Additionally, it can be used to connect directly to the pod's master given the podname.
 
-# Configuration
-Source information is configured via environment variables:
-
-```shell
-PODCONNECTOR_SENTINELCONFIGFILE = /path/to/config/file
-PODCONNECTOR_REDSKULLADDRESS = IP:PORT
-```
-
-Currently, Redskull support is awaiting completion of the Red Skull RPC server.
 
 # Usage
 
 
 ```shell
-pod-connector -podname <podname> [-cli]
+pod-connector [-sentinelconfig "/path/to/file"] (info [-j]|cli) podname
 ```
 
-If neither of the source environment variables are available, pod-connector
-will assume you want to use /etc/redis/sentinel.conf as the source.
-
-To be useful you must use the flag `-podname <podname>` or `-podname=<podname>`
-where '<podname>' is replaced by the name of the pod as it was registered in
-Sentinel via the `MONITOR` command
+By default the expected Sentinel config file is "/etc/redis/sentinel.conf"
 
 ## Info Dump
 
 By default it will dump whatever information is available from the source. You
-can choose to let it be formatted for humans, or pass `-jsonout` to get it as a
-JSON encoded string.
+can choose to let it be formatted for humans, or pass `-json` or `-j` to get it
+as a JSON encoded string.
 
 ## Connecting Directly
-If you pass the flag `-cli`, pod-connector will execute redis-cli on your
+If you use the `cli` command, pod-connector will execute redis-cli on your
 behalf with all needed information to connect to the master - including
 authentication. Essentially this saves you from needing to execute the
 redis-cli yourself. You will be placed directly into the redis-cli shell on a
@@ -56,10 +42,10 @@ sentinel and the pod. Different needs, different tools. This is much simpler
 than yet another user system in a tool.
 
 # TODO
-* Option to set various sentinel variables such as down-in-milliseconds
-* Update password option to update the password in sentinel (and optionally the instances?)
 * Ability to pass redis-cli arguments to pod-connector to have it execute for you
-* Implement Redskull RPC support
 
-The options to update might go into a seaprate tool 'pod-configurator' to
-better isolate capabilities.
+
+# Bash Completion
+
+Add the pod-connector_completion script to your completions directory and/or
+source it directly to add bash completion.
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 
 	"log"
@@ -10,6 +9,7 @@ import (
 	"os/exec"
 	"text/template"
 
+	"github.com/codegangsta/cli"
 	"github.com/kelseyhightower/envconfig"
 
 	parser "github.com/sentinel-tools/sconf-parser"
@@ -24,19 +24,23 @@ type LaunchConfig struct {
 }
 
 var (
-	config LaunchConfig
-	enc    *json.Encoder
+	pod      parser.PodConfig
+	err      error
+	config   LaunchConfig
+	enc      *json.Encoder
+	podname  string
+	cfile    string
+	app      *cli.App
+	sentinel parser.SentinelConfig
 )
 
 // cli flags
-var (
-	podname            string
-	cli                bool
-	info               bool
-	jsonout            bool
-	validatemasterrole bool
-)
 
+func SetConfigFile(c *cli.Context) error {
+	cfile = c.GlobalString("sentinelconfig")
+	sentinel, err = parser.ParseSentinelConfig(config.SentinelConfigFile)
+	return err
+}
 func init() {
 	err := envconfig.Process("podconnector", &config)
 	if err != nil {
@@ -77,66 +81,78 @@ func checkError(err error) {
 	}
 }
 
+func beforePodCommand(c *cli.Context) (err error) {
+	//podname := c.String("name")
+	args := c.Args()
+	if len(args) == 0 {
+		log.Fatal("Need a podname as first argument")
+	}
+	podname = args[0]
+	pod, _ = sentinel.GetPod(podname)
+	return nil
+}
 func main() {
-	flag.StringVar(&podname, "podname", "", "Name of the pod")
-	flag.BoolVar(&cli, "cli", false, "launch redis-cli to connect to the pod")
-	flag.BoolVar(&jsonout, "jsonout", false, "output info in JSON format (requires -info)")
-	flag.BoolVar(&info, "info", false, "output info")
-	flag.Parse()
+	app = cli.NewApp()
+	app.Name = "pod-connector"
+	app.Usage = "Interact with a Sentinel using configuration data"
+	app.Version = "0.5.1"
+	app.EnableBashCompletion = true
+	author := cli.Author{Name: "Bill Anderson", Email: "therealbill@me.com"}
+	app.Authors = append(app.Authors, author)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "sentinelconfig, s",
+			Value: "/etc/redis/sentinel.conf",
+			Usage: "Location of the sentinel config file",
+		},
+	}
+	app.Before = SetConfigFile
 
-	var pod parser.PodConfig
-	var err error
-	if podname == "" {
-		log.Print("Need a podname. Try using '-podname <podname>'")
-		flag.PrintDefaults()
-		return
+	app.Commands = []cli.Command{
+		{
+			Name:   "cli",
+			Usage:  "connect redis-cli to the pod",
+			Action: PodCli,
+			Before: beforePodCommand,
+		},
+		{
+			Name:   "info",
+			Usage:  "Show Sentinel information on the pod",
+			Action: ShowInfo,
+			Before: beforePodCommand,
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "json, j",
+					Usage: "Output only JSON",
+				},
+			},
+		},
 	}
-	if jsonout {
-		enc = json.NewEncoder(os.Stdout)
-	}
+	enc = json.NewEncoder(os.Stdout)
+	app.Run(os.Args)
+}
 
-	if config.UseSentinelConfig {
-		log.Print("using sentinel config file")
-		sentinel, err := parser.ParseSentinelConfig(config.SentinelConfigFile)
-		if err != nil {
-			log.Fatal(err)
+func ShowInfo(c *cli.Context) {
+	if c.Bool("json") {
+		if err := enc.Encode(&pod); err != nil {
+			log.Println(err)
 		}
-		pod, err = sentinel.GetPod(podname)
+	} else {
+		t := template.Must(template.New("podinfo").Parse(PodInfoTemplate))
+		err := t.Execute(os.Stdout, pod)
 		if err != nil {
-			log.Fatal(err)
+			log.Println("executing template:", err)
 		}
+		fmt.Printf("cli string: redis-cli -h %s -p %s -a %s\n", pod.MasterIP, pod.MasterPort, pod.Authpass)
 	}
-	if config.UseRedSkull {
-		log.Print("Using RedSkull connection")
-		pod, err = getPodInfoFromRedSkull(podname)
-		if err != nil {
-			fmt.Printf("Error: %s\n", err.Error())
-			return
-		}
-	}
-	if cli {
-		//args := flag.Args()
-		log.Printf("Connecting to pod '%s' via CLI", podname)
-		cmd := exec.Command("redis-cli", "-h", pod.MasterIP, "-p", pod.MasterPort, "-a", pod.Authpass)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Stdin = os.Stdin
-		cmd.Run()
-	}
+	return
+}
 
-	if info {
-		if jsonout {
-			if err := enc.Encode(&pod); err != nil {
-				log.Println(err)
-			}
-		} else {
-			t := template.Must(template.New("podinfo").Parse(PodInfoTemplate))
-			err := t.Execute(os.Stdout, pod)
-			if err != nil {
-				log.Println("executing template:", err)
-			}
-			fmt.Printf("cli string: redis-cli -h %s -p %s -a %s\n", pod.MasterIP, pod.MasterPort, pod.Authpass)
-		}
-		return
-	}
+func PodCli(c *cli.Context) {
+	log.Printf("Connecting to pod '%s' via CLI", podname)
+	cmd := exec.Command("redis-cli", "-h", pod.MasterIP, "-p", pod.MasterPort, "-a", pod.Authpass)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Run()
 }
